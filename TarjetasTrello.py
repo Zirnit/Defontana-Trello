e9f4e1a51fb0bf8a03abec0700334cd2a8692dfc
Zirnit <111541556+Zirnit@users.noreply.github.com>
2022-10-18 15:27:18 -0300

    Update TarjetasTrello.py

--- TarjetasTrello.py
+++ TarjetasTrello.py
@@ -1,27 +1,34 @@
 import requests
 import HeadersKeys as HK
 
-# Lista idList de Trello
-Lista1 = "{id}" # id de lista 1
-Lista2 = "{id}" # id de lista 2
-Lista3 = "{id}" # id de lista 3
-Lista4 = "{id}" # id de lista 4
+# Lista idList de Trello board "Pedidos"
+pendientes_idList = "61ce1d50882ab9559d105e44" #Pendientes Santiago
+playa_idList = "62f67cff9ba3ce0c4ffd4be2" # Pendientes playa
+monsalve_idList = "62f67d119efeba8106426433" # Pendientes Monsalve
+listo_idList = "61ce1d50882ab9559d105e46"
 
+# Lista labelList de Trello board "Pedidos"
+etiqueta_Playa = "62eaecd5dfd104854cc9ff3d"
+etiqueta_Monsalve = "62f691943b8e600bfc3bad29"
 
 # Create Trello card
-def post_trello(nombre, detalle, fecha):
+def post_trello(nombre, detalle, fechaC, fechaV, idLabels="false", idList=pendientes_idList):
     trelloCard = "https://api.trello.com/1/cards" # Dirección API
-    TrelloQS = {"key":"key","token":"token",
-    "idList":"idlist", # Lista del pedido en Trello
+    TrelloQS = {
+    "key":HK.tKey,
+    "token":HK.tToken,
+    "idList":idList, # Lista del pedido en Trello
     "name":nombre, # Nombre de la tarjeta
     "desc":detalle, # Descripción de la tarjeta
     "pos":"top", # Posición en la cual se crea la tarjeta (top, bottom, or a positive float)
-    "due": fecha} # Fecha de "caducidad" de la tarjeta
+    "start": fechaC, # Fecha de creación de la tarjeta
+    "due": fechaV, # Fecha de "caducidad" de la tarjeta
+    "idLabels": idLabels} # Etiqueta de la tarjeta
     requests.request("POST", trelloCard, headers=HK.trelloHeaders, params=TrelloQS)
 
 # Para obtener las ID de las tarjetas de Trello
 def lista_tarjetas_trello():
-    tarjetasTrelloURL = "https://api.trello.com/1/boards/{boardID}/cards" # filter Valid Values: all, closed, none, open, visible.
+    tarjetasTrelloURL = "https://api.trello.com/1/boards/61ce1d50882ab9559d105e43/cards" # filter Valid Values: all, closed, none, open, visible.
     IDtarjetasTrello = {}
     requestTarjetasTrello = requests.request(
     "GET",
@@ -37,8 +44,8 @@ def lista_tarjetas_trello():
 # Modificar tarjeta Trello
 def mod_trello(cardID, closed, idList):
     trelloCard = f"https://api.trello.com/1/cards/{cardID}" # Dirección API
-    querystring = {"key":"key",
-    "token":"token",
+    querystring = {"key":HK.tKey,
+    "token":HK.tToken,
     "closed": closed,
-    "idList": idList}
+    "idList": idList,}
     requests.put(trelloCard, headers=HK.trelloHeaders, params=querystring)
